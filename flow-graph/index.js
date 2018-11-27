848660834866be2ba91a033173339b7a32521268
Daler Asrorov <asrorids@gmail.com>
2018-11-27 00:22:54 -0800

    separate solution into module

--- flow-graph/index.js
+++ flow-graph/index.js
@@ -3,6 +3,3 @@ const data = require('./test-data');
 
 const result1 = buildFlowGraph(data.sample1);
 const result2 = buildFlowGraph(data.sample2);
-
-console.log({ result1 }, '\n');
-console.log({ result2 });
--- flow-graph/test-data.js
+++ flow-graph/test-data.js
@@ -50,12 +50,17 @@ module.exports = {
     {
       user_id: '300',
       timestamp: 1009,
-      action: 'B'
+      action: 'A'
     },
     {
       user_id: '100',
       timestamp: 1026,
       action: 'B'
+    },
+    {
+      user_id: '400',
+      timestamp: 1027,
+      action: 'C'
     },
     {
       user_id: '100',
@@ -68,8 +73,48 @@ module.exports = {
       action: 'B'
     },
     {
+      user_id: '300',
+      timestamp: 1119,
+      action: 'B'
+    },
+    {
+      user_id: '500',
+      timestamp: 1127,
+      action: 'C'
+    },
+    {
+      user_id: '400',
+      timestamp: 1200,
+      action: 'D'
+    },
+    {
+      user_id: '400',
+      timestamp: 1202,
+      action: 'F'
+    },
+    {
       user_id: '200',
       timestamp: 1503,
+      action: 'C'
+    },
+    {
+      user_id: '300',
+      timestamp: 1505,
+      action: 'F'
+    },
+    {
+      user_id: '200',
+      timestamp: 1507,
+      action: 'D'
+    },
+    {
+      user_id: '500',
+      timestamp: 1527,
+      action: 'F'
+    },
+    {
+      user_id: '500',
+      timestamp: 1537,
       action: 'A'
     }
   ]
