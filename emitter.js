ffe6cfa478852cf22a0999cec94afae9a3ce53e8
Daler Asrorov <asrorids@gmail.com>
2018-11-30 04:21:25 -0800

    Added emitter solution

---
+++ emitter.js
@@ -0,0 +1,51 @@
+class Emitter {
+  constructor() {
+    this.queue = {};
+  }
+
+  subscribe(event, fn) {
+    this.queue[event] = this.queue[event] ? [...this.queue[event], fn] : [fn];
+
+    return {
+      release: () => {
+        let fnIndex = this.queue[event].indexOf(fn);
+
+        for (let i = 0; i < this.queue[event].length; i++) {
+          if (fnIndex === i) {
+            this.queue[event].splice(i, 1);
+            break;
+          }
+        }
+      }
+    };
+  }
+
+  emit(event, ...paramArgs) {
+    this.queue[event].forEach(fn => {
+      fn.call(null, ...paramArgs);
+    });
+  }
+
+  release() {}
+}
+
+let emitter = new Emitter();
+
+let sub1 = emitter.subscribe('gaw', (name, lastname) =>
+  console.log(`I am a dog. Hi ${name} ${lastname}`)
+);
+let sub2 = emitter.subscribe('gaw', (name, lastname) =>
+  console.log(`I am a foo. Hi ${name} ${lastname}`)
+);
+
+let sub3 = emitter.subscribe('gaw', (name, lastname) =>
+  console.log(`I am a troo. Hi ${name} ${lastname}`)
+);
+
+emitter.emit('gaw', 'daler', 'asrorov');
+
+console.log(emitter);
+sub2.release();
+console.log(emitter);
+
+emitter.emit('gaw', 'durak', 'burak');
