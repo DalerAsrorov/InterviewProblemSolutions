cde7025bc9b4406433c4bbab474ebf1e9498673c
Daler Asrorov <asrorids@gmail.com>
2018-12-05 22:12:58 -0800

    Added recursive solution

--- flattenArray.js
+++ flattenArray.js
@@ -1,10 +1,9 @@
-const flattenArray = array => {
+const flattenArrayIter = array => {
   let result = [];
   let queue = [];
 
   for (let i = 0; i < array.length; i++) {
-    const elem = array[i];
-    queue = [elem];
+    queue = [array[i]];
 
     while (queue.length > 0) {
       const item = queue.shift();
@@ -20,12 +19,36 @@ const flattenArray = array => {
   return result;
 };
 
-let result = flattenArray([
+const flattenArrayIter2 = array => {
+  // make a copy of the original
+  let nodes = array.slice();
+  let result = [];
+
+  while (nodes.length > 0) {
+    let node = nodes.shift();
+
+    if (Array.isArray(node)) {
+      nodes = [...node, ...nodes];
+    } else {
+      result.push(node);
+    }
+  }
+
+  return result;
+};
+
+const flattenRecur = array => {
+  const flat = [].concat(...array);
+  return array.some(Array.isArray) ? flattenRecur(flat) : flat;
+};
+
+const TEST1 = [
   1,
-  [2, 3],
+  [[2, 3]],
   [4, 5],
   [6, 7],
   [{}, [[[8, 9], [[10, 11]]]], [12, 13]]
-]);
+];
 
-console.log({ result });
+let result1 = flattenArrayIter2(TEST1);
+let result2 = flattenRecur(TEST1);
